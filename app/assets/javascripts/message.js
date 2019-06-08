3d2c60014764e7c99d3b6ca3cb13cce87cc44e8f
yikezawa <y.ikezawa93@gmail.com>
2019-06-08 16:08:50 +0900

    投稿画面の修正
    非同期化に伴い、viewを修正。
    haml内にあった不要なタグを消し、jsにて非同期にメッセージを表示させるように修正した。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(function(){
+
+  function buildHTML(message){
+    var html = 
+    `<div class="contents__post">
+      <div class="contents__post__head">
+        <div class="user_name">
+          ${message.user_name}
+        </div>
+        <div class="date">
+          ${message.created_at}
+        </div>
+      </div>
+      `
+    if(message.content != null){
+      var content = `
+      <p class="contents__post__text">
+      ${message.content}
+      </p>
+      `
+      html = html + content;
+    }
+    if(message.image.url != null){
+      var image = `
+      <img class="contents__post__image" src="${message.image.url}"></img>
+      `
+      html = html + image;
+    }
+    return html + "</div>";
+  }
+
+  $(".new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href;
+    console.log(href);
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: "JSON",
+      processData: false,
+      contentType: false
+    }).done(function(data){
+      var html = buildHTML(data);
+      $(".contents").append(html);
+      $(".inputBox__text").val("");
+      $(".inputBox__label__input").val("");
+      $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight});
+    }).fail(function(){
+      alert("通信でエラーが発生しました。");
+    })
+  });
+})
